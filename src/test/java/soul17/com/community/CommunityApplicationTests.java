7919b46e0851929909d9e9ee3057b0c7bf3ec93d
phl <541061870@qq.com>
2020-07-27 00:18:48 +0800

    init respo
    Signed-off-by: phl <541061870@qq.com>

---
+++ src/test/java/soul17/com/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package soul17.com.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
